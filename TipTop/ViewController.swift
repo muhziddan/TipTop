8f5011f8bc355937b1abdd3bd8b5f2ac7dc1e310
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-05-12 12:04:56 +0700

    Initial Commit

---
+++ TipTop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipTop
+//
+//  Created by Muhammad Ziddan Hidayatullah on 12/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
