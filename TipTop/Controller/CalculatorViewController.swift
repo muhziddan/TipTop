7a5f10168edba894da6716e9e3b35887a08a9be4
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-05-18 06:53:59 +0700

    add calculate function

--- TipTop/Controller/CalculatorViewController.swift
+++ TipTop/Controller/CalculatorViewController.swift
@@ -15,18 +15,15 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPercentageButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    var value: Double?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    var bill: Double?
+    var tip: Double?
+    var split: Double = 1.0
 
     @IBAction func tipChanged(_ sender: UIButton) {
         zeroPercentageButton.isSelected = false
         tenPercentageButton.isSelected = false
         twentyPercentageButton.isSelected = false
+        billTextField.endEditing(true)
         
         sender.isSelected = true
         
@@ -34,13 +31,29 @@ class CalculatorViewController: UIViewController {
         
         let number = Double(buttonTitle ?? "0.0")
         
-        value = (number ?? 0.0) / 100
+        tip = (number ?? 0.0) / 100
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        split = sender.value
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print(value ?? 0.0)
+        guard let bill = billTextField.text else {
+            return
+        }
+        
+        guard let billDecimal = Double(bill) else {
+            return
+        }
+        
+        let totalBill = billDecimal * (1 + (tip ?? 0.0))
+        let afterSplit = round(totalBill / split * 100) / 100
+        
+        print(tip ?? 0.0)
+        print(billTextField.text ?? "")
+        print(split)
+        print(afterSplit)
     }
 }
