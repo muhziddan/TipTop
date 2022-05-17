deadcd7371b66436555f63a52999d876104e966f
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-05-17 20:27:31 +0700

    add new view controller and connect it to the storyboard

---
+++ TipTop/Controller/ResultsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultsViewController.swift
+//  TipTop
+//
+//  Created by Muhammad Ziddan Hidayatullah on 17/05/22.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+}
