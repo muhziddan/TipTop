c07a5a80ddca2e1988be4d4bb15c15fb848df93c
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-05-17 17:37:01 +0700

    add necessary IBOutlet and IBAction

---
+++ TipTop/CalculatorViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TipTop
+//
+//  Created by Muhammad Ziddan Hidayatullah on 12/05/22.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPercentageButton: UIButton!
+    @IBOutlet weak var tenPercentageButton: UIButton!
+    @IBOutlet weak var twentyPercentageButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+}
