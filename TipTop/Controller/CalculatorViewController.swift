fdcf584d8f7d3f588466658308aedfc604bde7ce
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-05-17 20:40:50 +0700

    if button selected, other button will not be selected and the button selected will print the same decimal value

--- TipTop/Controller/CalculatorViewController.swift
+++ TipTop/Controller/CalculatorViewController.swift
@@ -15,6 +15,8 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPercentageButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
+    var value: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,11 +24,23 @@ class CalculatorViewController: UIViewController {
 
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPercentageButton.isSelected = false
+        tenPercentageButton.isSelected = false
+        twentyPercentageButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        let buttonTitle = sender.titleLabel?.text?.dropLast()
+        
+        let number = Double(buttonTitle ?? "0.0")
+        
+        value = (number ?? 0.0) / 100
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        print(value ?? 0.0)
     }
 }
